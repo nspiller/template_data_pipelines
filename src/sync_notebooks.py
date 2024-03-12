04393aa8d026e5e7b6f9d34f4b54b96b11aa6f5e
Nico Spiller <nspiller1@mailbox.org>
2024-03-12 15:51:25 -0400

    updated readme, new notebooks handling

---
+++ src/sync_notebooks.py
@@ -0,0 +1,35 @@
+import subprocess
+from pathlib import Path
+
+
+def sync(notebook_folder, script_folder):
+    """Cycle through Jupyter notebooks and sync with paired Python scripts.
+
+    Parameters
+    ----------
+    notebook_folder : path-like
+        Path to folder where .ipynb files will be saved
+    """
+    notebook_files = Path(notebook_folder).glob("*.ipynb")
+    script_files = Path(script_folder).glob("*.py")
+    files = list(notebook_files) + list(script_files)
+
+    for f in files:
+        command = f"jupytext --sync {f}"
+        print(f">> RUNNING {command}")
+        subprocess.run(command.split())
+
+if __name__ == "__main__":
+
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description="Convert between Jupyter notebooks and Python scripts using jupytext.",
+        epilog="""
+        Requires pyproject.toml with jupytext configuration. 
+        IMPORTANT: does not handle white spaces in file names
+        """,
+    )
+
+    args = parser.parse_args()
+    sync("notebooks", "notebooks/scripts")
